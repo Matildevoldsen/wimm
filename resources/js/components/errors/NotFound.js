635561c43d577daecb93fe1e74ccbf8d48dfe5fd
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2019-10-01 18:17:22 +0100

    Made Not Found page more informative.

--- resources/js/components/errors/NotFound.js
+++ resources/js/components/errors/NotFound.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Select, Layout, Result, Button, Typography, Modal, Input } from "antd";
+import { Alert, Form, Select, Layout, Result, Button, Typography, Modal, Input } from "antd";
 import { Link } from "react-router-dom";
 
 const { Content } = Layout;
@@ -66,12 +66,14 @@ class NotFound extends Component {
                                     title="Submit Feedback"
                                     visible={this.state.visible}
                                     onOk={this.handleOk}
+                                    okButtonProps={{ disabled: true }}
+                                    okText="Submit"
                                     onCancel={this.handleCancel}
                                 >
                                     <Form onSubmit={this.handleSubmit}>
                                         <Typography.Paragraph>
                                             If you have any concerns or think
-                                            this site should exist let us know.
+                                            this page should exist let us know.
                                             If you have any other feedback to
                                             submit please feel free to send it
                                             here. Only leave your E-mail address if
@@ -82,9 +84,6 @@ class NotFound extends Component {
                                             <Select defaultValue="missingpage" disabled placeholder="Feedback type">
                                                 <Option value="missingpage" selected>
                                                     Missing Page
-                                                </Option>
-                                                <Option value="usa">
-                                                    U.S.A
                                                 </Option>
                                             </Select>
                                         </Form.Item>
@@ -100,6 +99,7 @@ class NotFound extends Component {
                                         <Form.Item label="Please entter your email">
                                             <Input placeholder="Please enter your email" icon="email" />
                                         </Form.Item>
+                                        <Alert message="Please ensure all required fields are not empty" type="info" showIcon />
                                     </Form>
                                 </Modal>
                             </div>
